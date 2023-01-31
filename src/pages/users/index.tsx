2b2f7c205577cb660ce1252d8aed6d3080f8c094
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2023-01-31 17:09:26 -0400

    Configurando DevTools

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -12,8 +12,21 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users')
     const data = await response.json()
 
-    return data
-  })
+    const users = data.users.map((user: any) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric'
+        }),
+      }
+    })
+
+    return users
+  }, { staleTime: 1000 * 5 })
 
 
   const isWideVersion = useBreakpointValue({
@@ -76,54 +89,25 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
+                  {data.map((user: any) => (
+                    <>
+                      <Tr key={user.id}>
+                        <Td px={["4", "4", "6"]}>
+                          <Checkbox colorScheme="pink" />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight="bold">{user.name}</Text>
+                            <Text fontSize="sm" color="gray.300">{user.email}</Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td>
 
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Lucas Oliveira</Text>
-                        <Text fontSize="sm" color="gray.300">lucas.schell.f@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                    <Td>
-
-                    </Td>
-                  </Tr>
-
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Lucas Oliveira</Text>
-                        <Text fontSize="sm" color="gray.300">lucas.schell.f@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                    <Td>
-
-                    </Td>
-                  </Tr>
-
-                  <Tr>
-                    <Td px={["4", "4", "6"]}>
-                      <Checkbox colorScheme="pink" />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight="bold">Lucas Oliveira</Text>
-                        <Text fontSize="sm" color="gray.300">lucas.schell.f@gmail.com</Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                    <Td>
-
-                    </Td>
-                  </Tr>
+                        </Td>
+                      </Tr>
+                    </>
+                  ))}
 
                 </Tbody>
               </Table>
