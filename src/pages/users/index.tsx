59669cb8dae5513cacb0b76721c13018dc1ac98d
Lucas Barbosa Cesar de Oliveira <lucascoliveira819@gmail.com>
2023-02-01 16:28:03 -0400

    Prefetch de dados

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,8 +1,10 @@
 
 import { Pagination } from '@/components/Pagination';
 import { Sidebar } from '@/components/Sidebar';
+import { api } from '@/services/api';
 import { useUsers } from '@/services/hooks/useUsers';
-import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue } from '@chakra-ui/react';
+import { queryClient } from '@/services/queryClient';
+import { Box, Button, Checkbox, Flex, Heading, Icon, Spinner, Table, Tbody, Td, Text, Th, Thead, Tr, useBreakpointValue, Link as ChakraLink } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useState } from 'react';
 import { RiAddLine } from 'react-icons/ri';
@@ -16,6 +18,16 @@ export default function UserList() {
     base: false,
     lg: true
   })
+
+  async function handlePrefetchUser(userId: number) {
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`)
+      return response.data
+    },
+      {
+        staleTime: 1000 * 60 * 10 // 10 minutes
+      })
+  }
 
   return (
     <Box>
@@ -82,7 +94,10 @@ export default function UserList() {
                         </Td>
                         <Td>
                           <Box>
-                            <Text fontWeight="bold">{user.name}</Text>
+                            <ChakraLink color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                              <Text fontWeight="bold">{user.name}</Text>
+                            </ChakraLink>
+
                             <Text fontSize="sm" color="gray.300">{user.email}</Text>
                           </Box>
                         </Td>
--- src/services/hooks/useUsers.ts
+++ src/services/hooks/useUsers.ts
@@ -43,6 +43,6 @@ async function getUsers(page: number): Promise<GetUsersResponse> {
 }
 
 export function useUsers(page: number) {
-  return useQuery(['users', page], () => getUsers(page), { staleTime: 1000 * 5 })
+  return useQuery(['users', page], () => getUsers(page), { staleTime: 1000 * 60 * 10 })
 
 }
